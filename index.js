a58f3c62a86e71302237733180674aba57f4937f
Lugeja <aaronalev@gmail.com>
2024-06-08 12:58:37 +0300

    Displayed author article list

--- index.js
+++ index.js
@@ -54,6 +54,17 @@ app.get('/article/:slug', (req, res) => {
     });
 });
 
+app.get('/author/:id', (req, res) => {
+    let query = `SELECT author.name AS author, article.name AS article, article.slug AS slug, article.image AS image FROM article INNER JOIN author ON article.author_id = author.id WHERE author.id = ${req.params.id}`;
+    let articles = [];
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result;
+        author = result[0].author;
+        res.render('author' , {articles: articles, author: author});
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
 });
