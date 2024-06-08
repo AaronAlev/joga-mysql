b9f6ceb9cbd53350b139395e0082253f20b5a966
Lugeja <aaronalev@gmail.com>
2024-06-08 12:27:08 +0300

    Displayed an article by slug

--- index.js
+++ index.js
@@ -40,7 +40,16 @@ app.get('/', (req, res) => {
         articles = result;
         res.render('index' , {articles: articles});
     })
+});
 
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug = '${req.params.slug}'`;
+    let article;
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result;
+        res.render('article' , {article: article});
+    });
 });
 
 app.listen(3000, () => {
